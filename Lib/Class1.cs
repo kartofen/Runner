85cd23800190c6eabdc34cfa19274770960df7ff
DESKTOP-7BCU5G4\112 <mladenovnasko0@gmail.com>
2020-08-26 22:27:49 +0300

    a lot adder, soon ready

--- Lib/Class1.cs
+++ Lib/Class1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using Console = Colorful.Console;
 
 namespace Lib
 {
     public class Class1
     {
+        public static int highscore = 0;
+
         public static void WritingLevels(string[,] levels)
         {
             int b = 0;
@@ -12,6 +15,7 @@ namespace Lib
                 CenterText("│" +" "+ levels[i, b] + levels[i, b + 1] + levels[i, b + 2] + levels[i, b + 3] + levels[i, b + 4] + levels[i, b + 5] + levels[i, b + 6] + levels[i, b  + 7] + levels[i, b + 8] + levels[i, b + 9] + levels[i, b + 10] +" "+ "│");
             }
         }
+
         public static void CenterText(string text)
         {
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
@@ -19,9 +23,9 @@ namespace Lib
 
         public static void Replacing(string[,] source, int sourceRow, string[,] target, int targetRow)
         {
-            for(int a = 0; a < 11; a++)
+            for(int i = 0; i < 11; i++)
             {
-                target[targetRow, a] = source[sourceRow, a];
+                target[targetRow, i] = source[sourceRow, i];
             }
         }
 
@@ -31,27 +35,129 @@ namespace Lib
             level0[coords[0, 2], coords[0, 3]] = "#";
             level0[coords[0, 4], coords[0, 5]] = "o";
             level0[coords[1, 0], coords[1, 1]] = "[";
-            level0[coords[1, 2], coords[1, 3]] = "C";
+            level0[coords[1, 2], coords[1, 3]] = "@";
             level0[coords[1, 4], coords[1, 5]] = "]";
             level0[coords[2, 0], coords[2, 1]] = "o";
-            level0[coords[2, 2], coords[2, 3]] = "@";
+            level0[coords[2, 2], coords[2, 3]] = "#";
             level0[coords[2, 4], coords[2, 5]] = "o";
         }
 
-        public static string IfGameIsOver(int[,] coords, string[,] level)
+        public static void IfGameIsOver(int[,] coords, string[,] level, bool AreYouMoving, bool AreYouMovingLeft)
         {
-            bool AreYouAlive = level[coords[0, 0] - 1, coords[0, 1]] == " " &&  level[coords[0, 2] - 1, coords[0, 3]] == " " && level[coords[0, 4] - 1, coords[0, 5]] == " ";
-            if(AreYouAlive == true)
-            { return ""; } 
+            if(AreYouMoving == true)
+            {
+                if(AreYouMovingLeft == true)
+                {
+                    if(level[coords[0, 0] , coords[0, 1] - 1] != " ")
+                    {
+                        IfYouHaveSomethingOnSide(level, coords, 0, 0, "left");
+                    }
+                    if(level[coords[1, 0] , coords[1, 1] - 1] != " ")
+                    {
+                        IfYouHaveSomethingOnSide(level, coords, 1, 0, "left");
+                    }
+                    if(level[coords[2, 0] , coords[2, 1] - 1] != " ")
+                    {
+                        IfYouHaveSomethingOnSide(level, coords, 2, 0, "left");
+                    }
+                }
+                if(AreYouMovingLeft == false)
+                {
+                    if(level[coords[0, 4] , coords[0, 5] + 1] != " ")
+                    {
+                        IfYouHaveSomethingOnSide(level, coords, 0, 4, "right");
+                    }
+                    if(level[coords[1, 4] , coords[1, 5] + 1] != " ")
+                    {
+                        IfYouHaveSomethingOnSide(level, coords, 1, 4, "right");
+                    }
+                    if(level[coords[2, 4] , coords[2, 5] + 1] != " ")
+                    {
+                        IfYouHaveSomethingOnSide(level, coords, 2, 4, "right");                   
+                    }
+                }
+            }
+            if(level[coords[0, 0] - 1, coords[0, 1]] != " ")
+            {
+                IfYouHaveSomethingInFront(level, coords, 0, 0);
+            }
+            if(level[coords[0, 2] - 1, coords[0, 3]] != " ")
+            {
+                IfYouHaveSomethingInFront(level, coords, 0, 2);        
+            }
+            if(level[coords[0, 4] - 1, coords[0, 5]] != " ")
+            {
+                IfYouHaveSomethingInFront(level, coords, 0, 4);   
+            }
+            return;
+        }
+
+        public static void IfYouHaveSomethingInFront(string[,] level, int[,] coords, int coordRow, int coordColumn)
+        {
+            if(level[coords[coordRow, coordColumn] - 1, coords[coordRow, coordColumn + 1]] == "$")
+            {
+                highscore++;
+            }
+            else if(level[coords[coordRow, coordColumn] - 1, coords[coordRow, coordColumn + 1]] == "|")
+            {
+            }
             else
             {
-                //if(level[coords[0, 0] - 1, coords[0, 1]] ==)
-                //temp comment ^
-
-                return "";
-                //temp ^
+                GameOver();
             }
+            return;
         }
-    }
+
+        public static void IfYouHaveSomethingOnSide(string[,] level, int[,] coords, int coordRow, int coordColumn, string selection)
+        {
+            switch (selection)
+            {
+                case "left":
+                    if(level[coords[coordRow, coordColumn], coords[coordRow, coordColumn + 1] - 1] == "$")
+                    {
+                        highscore++;
+                    }
+                    else if(level[coords[coordRow, coordColumn], coords[coordRow, coordColumn + 1] - 1] == "|")
+                    {
+                    }
+                    else
+                    {
+                        GameOver();
+                    }
+                break;
+                case "right":
+                    if(level[coords[coordRow, coordColumn], coords[coordRow, coordColumn + 1] + 1] == "$")
+                    {
+                        highscore++;
+                    }
+                    else if(level[coords[coordRow, coordColumn], coords[coordRow, coordColumn + 1] + 1] == "|")
+                    {
+                    }
+                    else
+                    {
+                        GameOver();
+                    }
+                break;
+            }
+            return;
+        }
+
+        public static void ReplaceFullArrays(string[,] newlevel, string[,] level0back)
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                Replacing(newlevel, i, level0back, i);
+            }
+            return;
+        }
+
+        public static void GameOver()
+        {
+            Class1.CenterText("Game Over!");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
         
+    }
 }
--- RunnerCode/Program.cs
+++ RunnerCode/Program.cs
@@ -20,7 +20,7 @@ namespace RunnerCode
             Game:
             {
                 prespective - from top
-                how the game works - every 2 second a layer replaces the layer under it
+                how the game works - every half a second a layer replaces the layer under it
                 looks like - you go up
             }
 
@@ -28,6 +28,7 @@ namespace RunnerCode
             {
                 make 2d arrays,
                 choose a 2d array randomly after the old array is played,
+                make 3 tyoes of levels - 20 layers            
             }
 
             Challanges:
@@ -64,29 +65,63 @@ namespace RunnerCode
                 ?? http://colorfulconsole.com/ - colors and things
             }
         */
-        /*todo to add
-            
-            Now adding:
-            {
-                cheking if the game is over
-                !! to be able to get money and die with the sides and front
-            }
+        /*todo to add  
 
             Things to add:
             {
-                move the car
-                get money/highscore
                 add more levels
-                add menu and how to play pages
-
             }
         */
 
         public static ConsoleKeyInfo cki;
 
+        public static string[,] level3 = {
+            {" ", " ", " ", "|", " ", "end", " ", "|", " ", " ", " "},
+            {" ", "$", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", "o", "&", "o", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", "[", "V", " ", "|", "[", "P", "]"},
+            {" ", " ", " ", "|", "o", "&", "o", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", "$", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", "$", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", " ", "$", " ", "|", "[", "P", "]"},
+            {" ", " ", " ", "|", " ", "$", " ", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", " ", "$", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+        };
+        public static string[,] level2 = {
+            {" ", " ", " ", "|", " ", "end", " ", "|", " ", " ", " "},
+            {" ", "$", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", "o", "&", "o", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", "[", "V", " ", "|", "[", "P", "]"},
+            {" ", " ", " ", "|", "o", "&", "o", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", "$", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", "$", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", " ", " ", " ", "|", "[", "P", "]"},
+            {" ", " ", " ", "|", " ", "$", " ", "|", "o", "&", "o"},
+            {" ", " ", " ", "|", " ", "$", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+        };
         public static string[,] level1 = {
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", "end", " ", "|", " ", " ", " "},
+            {" ", "$", " ", "|", " ", "$", " ", "|", " ", " ", " "},
             {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
             {"o", "&", "o", "|", " ", " ", " ", "|", "o", "&", "o"},
             {"[", "V", "]", "|", " ", " ", " ", "|", "[", "P", "]"},
@@ -106,47 +141,118 @@ namespace RunnerCode
             {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
             {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
         };
+
+        public static string[,] level0back = {
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+        };
         public static string[,] level0 = {
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
             {" ", " ", " ", "|", " ", "$", " ", "|", " ", " ", " "},
-            {" ", " ", " ", "|", " ", " ", " ", "|", "o", "&", "o"},
-            {" ", " ", " ", "|", " ", " ", " ", "|", "[", "N", "]"},
-            {" ", " ", " ", "|", " ", " ", " ", "|", "o", "&", "o"},
+            {"R", "U", "N", "|", " ", " ", " ", "|", "N", "E", "R"},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", "$", " ", "|", " ", " ", " ", "|", " ", "$", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
             {" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "},
             {" ", " ", " ", "|", "o", "#", "o", "|", " ", " ", " "},
-            {" ", " ", " ", "|", "[", "C", "]", "|", " ", " ", " "},
-            {" ", " ", " ", "|", "o", "@", "o", "|", " ", " ", " "},
+            {" ", " ", " ", "|", "[", "@", "]", "|", " ", " ", " "},
+            {" ", " ", " ", "|", "o", "#", "o", "|", " ", " ", " "},
         };
         
         //every pair of two ints represent a car's char position, first is the row then the column
         public static int[,] YouCarCoords = {
             {7, 4 /*o*/, 7, 5 /*#*/, 7, 6 /*o*/},
             {8, 4 /*[*/, 8, 5 /*C*/, 8, 6 /*]*/},
-            {9, 4 /*o*/, 9, 5 /*@*/, 9, 6 /*o*/}
+            {9, 4 /*o*/, 9, 5 /*#*/, 9, 6 /*o*/}
         };
-
-        public static int highscore = 0;
         static void Main(string[] args)
         {
             Menu();
         }
+
         static void Menu()
         {
             Console.CursorVisible = false;
+            Console.Clear();
+            Console.WriteAscii("         Runner");
+            Lib.Class1.CenterText("┌─────────────────┐");
+            Lib.Class1.CenterText("│ 1 - Start Game  │");
+            Lib.Class1.CenterText("│ 2 - How To Play │");
+            Lib.Class1.CenterText("│                 │");
+            Lib.Class1.CenterText("└─────────────────┘");
+            var key = Console.ReadKey();
+            
+            if(key.KeyChar == '1')
+            {
+                StartGame();
+            }
+            else if(key.KeyChar == '2')
+            {
+                HowToPlay();
+            }
+            else
+            {
+                Menu();
+            }
+        }
+
+        public static void HowToPlay()
+        {
+            Console.Clear();
+            Console.WriteAscii("         Runner");
+            Console.WriteLine("");
+            Lib.Class1.CenterText("┌───────────────────┐");      
+            Lib.Class1.CenterText("│  The car with C   │");
+            Lib.Class1.CenterText("│ is you            │");
+            Lib.Class1.CenterText("│                   │"); 
+            Lib.Class1.CenterText("│  Use the arrow    │");
+            Lib.Class1.CenterText("│ keys to move left │");
+            Lib.Class1.CenterText("│ and right         │");
+            Lib.Class1.CenterText("│                   │");
+            Lib.Class1.CenterText("│  Try to get as    │");
+            Lib.Class1.CenterText("│ much money($) as  │");
+            Lib.Class1.CenterText("│ you can           │");
+            Lib.Class1.CenterText("│                   │");
+            Lib.Class1.CenterText("└───────────────────┘");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void StartGame()
+        {
+            Console.CursorVisible = false;
+            
             while (1<2)
             {
-                Console.Clear();
+                Console.Clear(); 
                 Console.WriteAscii("         Runner");
                 Console.WriteLine("");
-                Lib.Class1.CenterText("HighScore:" + highscore);
+                Lib.Class1.CenterText("HighScore:" + Class1.highscore);
                 Lib.Class1.CenterText("┌─────────────┐");
                 Lib.Class1.WritingLevels(level0);
                 Lib.Class1.CenterText("└─────────────┘");
                 Console.WriteLine("");
-                IfGameIsOver();
+                IfGameIsOver(false, false);
                 ListenForKeys();
-                Thread.Sleep(300);
+                Thread.Sleep(425);
                 ListenForKeys();
                 MovingBackground();
             }
@@ -159,11 +265,11 @@ namespace RunnerCode
                 cki = Console.ReadKey();
                 if(cki.Key == ConsoleKey.LeftArrow)
                 {
-                    MovingYou("left");
+                    MovingLeft();
                 }
                 if(cki.Key == ConsoleKey.RightArrow)
                 {
-                    MovingYou("right");
+                    MovingRight();
                 }
             }
             return;
@@ -171,6 +277,30 @@ namespace RunnerCode
 
         static void MovingBackground()
         {
+            bool Haslevel1Ended = 
+            level0back[0, 0] == level0back[19, 0] && level0back[0, 1] == level0back[19, 1] && level0back[0, 2] == level0back[19, 2] && level0back[0, 3] == level0back[19, 3] &&
+            level0back[0, 4] == level0back[19, 4] && level0back[0, 5] == level0back[19, 5] && level0back[0, 6] == level0back[19, 6] && level0back[0, 7] == level0back[19, 7] && level0back[0, 8] == level0back[19, 8] &&
+            level0back[0, 9] == level0back[19, 9] ;
+
+            if(Haslevel1Ended == true)
+            {
+                Random rand = new Random();
+                int randIndex = rand.Next(1, 4);
+                switch(randIndex)
+                {
+                    case 1:
+                        Lib.Class1.ReplaceFullArrays(level1, level0back);
+                    break;
+                    case 2:
+                        Lib.Class1.ReplaceFullArrays(level2, level0back);
+                    break;
+                    case 3:
+                        Lib.Class1.ReplaceFullArrays(level3, level0back);
+                    break;
+                }
+                
+            }
+           
             Class1.Replacing(level0, 8, level0, 9);
             Class1.Replacing(level0, 7, level0, 8);
             Class1.Replacing(level0, 6, level0, 7);
@@ -181,57 +311,110 @@ namespace RunnerCode
             Class1.Replacing(level0, 1, level0, 2);
             Class1.Replacing(level0, 0, level0, 1);
 
-            Class1.Replacing(level1, 19, level0, 0);
-            Class1.Replacing(level1, 18, level1, 19);
-            Class1.Replacing(level1, 17, level1, 18);
-            Class1.Replacing(level1, 16, level1, 17);
-            Class1.Replacing(level1, 15, level1, 16);
-            Class1.Replacing(level1, 14, level1, 15);
-            Class1.Replacing(level1, 13, level1, 14);
-            Class1.Replacing(level1, 12, level1, 13);
-            Class1.Replacing(level1, 11, level1, 12);
-            Class1.Replacing(level1, 10, level1, 11);
-            Class1.Replacing(level1, 9, level1, 10); 
-            Class1.Replacing(level1, 8, level1, 9);
-            Class1.Replacing(level1, 7, level1, 8);
-            Class1.Replacing(level1, 6, level1, 7);
-            Class1.Replacing(level1, 5, level1, 6);
-            Class1.Replacing(level1, 4, level1, 5);
-            Class1.Replacing(level1, 3, level1, 4);
-            Class1.Replacing(level1, 2, level1, 3);
-            Class1.Replacing(level1, 1, level1, 2);
-            Class1.Replacing(level1, 0, level1, 1);
+            Class1.Replacing(level0back, 19, level0, 0);
+            Class1.Replacing(level0back, 18, level0back, 19);
+            Class1.Replacing(level0back, 17, level0back, 18);
+            Class1.Replacing(level0back, 16, level0back, 17);
+            Class1.Replacing(level0back, 15, level0back, 16);
+            Class1.Replacing(level0back, 14, level0back, 15);
+            Class1.Replacing(level0back, 13, level0back, 14);
+            Class1.Replacing(level0back, 12, level0back, 13);
+            Class1.Replacing(level0back, 11, level0back, 12);
+            Class1.Replacing(level0back, 10, level0back, 11);
+            Class1.Replacing(level0back, 9, level0back, 10); 
+            Class1.Replacing(level0back, 8, level0back, 9);
+            Class1.Replacing(level0back, 7, level0back, 8);
+            Class1.Replacing(level0back, 6, level0back, 7);
+            Class1.Replacing(level0back, 5, level0back, 6);
+            Class1.Replacing(level0back, 4, level0back, 5);
+            Class1.Replacing(level0back, 3, level0back, 4);
+            Class1.Replacing(level0back, 2, level0back, 3);
+            Class1.Replacing(level0back, 1, level0back, 2);
+            Class1.Replacing(level0back, 0, level0back, 1);
+
+            
             Class1.YouCarRepositioning(YouCarCoords, level0);
             return;
 
         }
 
-        static void MovingYou(string direction)
+        private static void MovingLeft()
         {
-            return;
-        }
-
-        static void IfGameIsOver()
-        {
-            switch (Class1.IfGameIsOver(YouCarCoords, level0))
+            if (YouCarCoords[0, 1] > 0)
             {
-                case "dead":
-                    GameOver();
-                break;
-
-                case "money":
-                    highscore =+ 1;
-                    return;
-
-                default:
-                    return;
+                IfGameIsOver(true, true);
+                if (YouCarCoords[0, 5] == 3)
+                {
+                    level0[YouCarCoords[0, 4], YouCarCoords[0, 5]] = "|";
+                    level0[YouCarCoords[1, 4], YouCarCoords[1, 5]] = "|";
+                    level0[YouCarCoords[2, 4], YouCarCoords[2, 5]] = "|";
+                }
+                else if (YouCarCoords[0, 5] == 7)
+                {
+                    level0[YouCarCoords[0, 4], YouCarCoords[0, 5]] = "|";
+                    level0[YouCarCoords[1, 4], YouCarCoords[1, 5]] = "|";
+                    level0[YouCarCoords[2, 4], YouCarCoords[2, 5]] = "|";
+                }
+                else
+                {
+                    level0[YouCarCoords[0, 4], YouCarCoords[0, 5]] = " ";
+                    level0[YouCarCoords[1, 4], YouCarCoords[1, 5]] = " ";
+                    level0[YouCarCoords[2, 4], YouCarCoords[2, 5]] = " ";
+                }
+                YouCarCoords[0, 1] = YouCarCoords[0, 1] - 1;
+                YouCarCoords[0, 3] = YouCarCoords[0, 3] - 1;
+                YouCarCoords[0, 5] = YouCarCoords[0, 5] - 1;
+                YouCarCoords[1, 1] = YouCarCoords[1, 1] - 1;
+                YouCarCoords[1, 3] = YouCarCoords[1, 3] - 1;
+                YouCarCoords[1, 5] = YouCarCoords[1, 5] - 1;
+                YouCarCoords[2, 1] = YouCarCoords[2, 1] - 1;
+                YouCarCoords[2, 3] = YouCarCoords[2, 3] - 1;
+                YouCarCoords[2, 5] = YouCarCoords[2, 5] - 1;
             }
             return;
         }
 
-        static void GameOver()
+        static void MovingRight()
         {
-            Class1.CenterText("Game Over!");
+            if (YouCarCoords[0, 5] < 10)
+            {
+                IfGameIsOver(true, false);
+                if (YouCarCoords[0, 1] == 3)
+                {
+                    level0[YouCarCoords[0, 0], YouCarCoords[0, 1]] = "|";
+                    level0[YouCarCoords[1, 0], YouCarCoords[1, 1]] = "|";
+                    level0[YouCarCoords[2, 0], YouCarCoords[2, 1]] = "|";
+                }
+                else if (YouCarCoords[0, 1] == 7)
+                {
+                    level0[YouCarCoords[0, 0], YouCarCoords[0, 1]] = "|";
+                    level0[YouCarCoords[1, 0], YouCarCoords[1, 1]] = "|";
+                    level0[YouCarCoords[2, 0], YouCarCoords[2, 1]] = "|";
+                }
+                else
+                {
+                    level0[YouCarCoords[0, 0], YouCarCoords[0, 1]] = " ";
+                    level0[YouCarCoords[1, 0], YouCarCoords[1, 1]] = " ";
+                    level0[YouCarCoords[2, 0], YouCarCoords[2, 1]] = " ";
+                }
+
+                YouCarCoords[0, 1] = YouCarCoords[0, 1] + 1;
+                YouCarCoords[0, 3] = YouCarCoords[0, 3] + 1;
+                YouCarCoords[0, 5] = YouCarCoords[0, 5] + 1;
+                YouCarCoords[1, 1] = YouCarCoords[1, 1] + 1;
+                YouCarCoords[1, 3] = YouCarCoords[1, 3] + 1;
+                YouCarCoords[1, 5] = YouCarCoords[1, 5] + 1;
+                YouCarCoords[2, 1] = YouCarCoords[2, 1] + 1;
+                YouCarCoords[2, 3] = YouCarCoords[2, 3] + 1;
+                YouCarCoords[2, 5] = YouCarCoords[2, 5] + 1;
+            }
+            return;
+        }
+
+        static void IfGameIsOver(bool AreYouMoving, bool AreMovingLeft)
+        {
+            Class1.IfGameIsOver(YouCarCoords, level0, AreYouMoving, AreMovingLeft);
+            return;
         }
     }
 }
